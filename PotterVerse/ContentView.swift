4e4cb3f9d61988c1a1bc5946a7a47869cd453da7
YoussefAlaaeldin <yousufalaa10@yahoo.com>
2025-10-14 15:06:09 +0300

    Initial Commit

---
+++ PotterVerse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PotterVerse
+//
+//  Created by Yousuf Abdelfattah on 14/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PotterVerse/PotterVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PotterVerseApp.swift
+//  PotterVerse
+//
+//  Created by Yousuf Abdelfattah on 14/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PotterVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
