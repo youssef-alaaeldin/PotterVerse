061c369dba8f42f593f93b3ef37d77a50d1435c9
YoussefAlaaeldin <yousufalaa10@yahoo.com>
2025-10-15 15:25:14 +0300

    Add OnboardingScreen

--- PotterVerse/App/RootView.swift
+++ PotterVerse/App/RootView.swift
@@ -17,7 +17,7 @@ struct RootView: View {
     
     var body: some View {
         ZStack {
-            
+            OnboardingView()
         }
 //        ZStack {
 //            if appState.currentView == .onboarding {
--- PotterVerse/Features/Onboarding/Presentation/View/OnboardingView.swift
+++ PotterVerse/Features/Onboarding/Presentation/View/OnboardingView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    @State private var upAndDownAnimation: Bool = false
+    
     var body: some View {
         ZStack {
             Image(.hogwarts)
@@ -15,13 +17,46 @@ struct OnboardingView: View {
                 .aspectRatio(contentMode: .fill)
                 .ignoresSafeArea()
             
-            VStack {
+            Rectangle()
+                .opacity(0.5)
+                .ignoresSafeArea()
+            
+            VStack(spacing: 12) {
                 Text("POTTERVERSE")
                     .font(.heading1)
                     .foregroundStyle(.primaryGold)
                     .fontWeight(.heavy)
+                
+                Text("Explore The Wizarding World")
+                    .font(.title3)
+                    .foregroundStyle(.white)
+                
+                HStack {
+                    Circle()
+                        .frame(width: 6, height: 6)
+                    
+                    Circle()
+                        .frame(width: 6, height: 6)
+                    
+                    Circle()
+                        .frame(width: 6, height: 6)
+                }
+                .foregroundStyle(.primaryGold)
+                .padding(.top, 14)
+                .offset(y: upAndDownAnimation ? -6 : 6)
+                .animation(.easeIn(duration: 1.2).repeatForever(autoreverses: true), value: upAndDownAnimation)
+                
+                CustomButton(title: "Enter the Magic") {
+                    
+                }
+                .padding(.top, 24)
             }
             .padding(.horizontal, 24)
+        }
+        .onAppear {
+            withAnimation {
+                upAndDownAnimation.toggle()
+            }
         }
     }
 }
