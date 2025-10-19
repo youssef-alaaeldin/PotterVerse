0f298156b60d287a12c2faeb6c101d23fd65c750
YoussefAlaaeldin <yousufalaa10@yahoo.com>
2025-10-19 14:13:16 +0300

    Add GoldGlowShadow Modifier

---
+++ PotterVerse/Utils/Components/GoldGlowShadow.swift
@@ -0,0 +1,23 @@
+//
+//  GoldGlowShadow.swift
+//  PotterVerse
+//
+//  Created by Yousuf Abdelfattah on 19/10/2025.
+//
+
+import SwiftUI
+
+struct GoldGlowShadow: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .shadow(color: Color.primaryGold.opacity(0.3),
+                    radius: 10, x: 0, y: 0)
+    }
+}
+
+
+extension View {
+    func goldGlowShadow() -> some View {
+        self.modifier(GoldGlowShadow())
+    }
+}
