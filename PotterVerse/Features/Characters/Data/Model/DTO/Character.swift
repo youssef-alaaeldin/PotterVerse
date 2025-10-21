13aaf74d339c84e41d3a788bc0212c0464a2bdc7
YoussefAlaaeldin <yousufalaa10@yahoo.com>
2025-10-21 15:11:27 +0300

    Add Character Model DTO

---
+++ PotterVerse/Features/Characters/Data/Model/DTO/Character.swift
@@ -0,0 +1,43 @@
+//
+//  Character.swift
+//  PotterVerse
+//
+//  Created by Yousuf Abdelfattah on 21/10/2025.
+//
+
+import Foundation
+
+struct Character: Codable {
+    let id, name: String
+    let alternateNames: [String]
+    let species: String
+    let gender: String?
+    let house: String?
+    let dateOfBirth: String?
+    let yearOfBirth: Int?
+    let wizard: Bool
+    let ancestry: String?
+    let eyeColour: String?
+    let hairColour: String?
+    let wand: Wand
+    let patronus: String?
+    let hogwartsStudent, hogwartsStaff: Bool
+    let actor: String
+    let alternateActors: [String]
+    let alive: Bool
+    let image: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, name
+        case alternateNames = "alternate_names"
+        case species, gender, house, dateOfBirth, yearOfBirth, wizard, ancestry, eyeColour, hairColour, wand, patronus, hogwartsStudent, hogwartsStaff, actor
+        case alternateActors = "alternate_actors"
+        case alive, image
+    }
+}
+// MARK: - Wand
+struct Wand: Codable {
+    let wood: String
+    let core: String?
+    let length: Double?
+}
