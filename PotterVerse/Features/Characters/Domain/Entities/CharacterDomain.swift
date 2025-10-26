363193d8c91067e4727758851667c534d06b11b7
YoussefAlaaeldin <yousufalaa10@yahoo.com>
2025-10-26 19:49:33 +0300

    Change Character Domain

--- PotterVerse/Features/Characters/Domain/Entities/CharacterDomain.swift
+++ PotterVerse/Features/Characters/Domain/Entities/CharacterDomain.swift
@@ -8,24 +8,10 @@
 import Foundation
 
 struct CharacterDomain {
-    let id, name: String
-    let alternateNames: [String]
-    let species: String
-    let gender: String
-    let house: String
-    let dateOfBirth: String
-    let yearOfBirth: Int
-    let wizard: Bool
-    let ancestry: String
-    let eyeColour: String
-    let hairColour: String
-    let wand: Wand
-    let patronus: String
-    let hogwartsStudent, hogwartsStaff: Bool
-    let actor: String
-    let alternateActors: [String]
-    let alive: Bool
+    let fullName, nickname: String
+    let hogwartsHouse: String
     let image: String
+    let birthdate: String
 }
 
 // MARK: - Mock Data
