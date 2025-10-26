75b7758cd60ab6e1ad3cc72a8507e7758475674d
YoussefAlaaeldin <yousufalaa10@yahoo.com>
2025-10-26 19:50:13 +0300

    Change Mapper

--- PotterVerse/Features/Characters/Data/Mapper/CharactersMapper.swift
+++ PotterVerse/Features/Characters/Data/Mapper/CharactersMapper.swift
@@ -10,27 +10,13 @@ import Foundation
 struct CharactersMapper: RemoteMapperProtocol {
     func dtoToDomain(_ model: CharacterDTO) -> CharacterDomain {
         return CharacterDomain(
-            id: model.id,
-            name: model.name,
-            alternateNames: model.alternateNames,
-            species: model.species,
-            gender: model.gender ?? "",
-            house: model.house ?? "",
-            dateOfBirth: model.dateOfBirth ?? "",
-            yearOfBirth: model.yearOfBirth ?? 0,
-            wizard: model.wizard,
-            ancestry: model.ancestry ?? "",
-            eyeColour: model.eyeColour ?? "",
-            hairColour: model.hairColour ?? "",
-            wand: model.wand,
-            patronus: model.patronus ?? "",
-            hogwartsStudent: model.hogwartsStudent,
-            hogwartsStaff: model.hogwartsStaff,
-            actor: model.actor,
-            alternateActors: model.alternateActors,
-            alive: model.alive,
-            image: model.image
+            fullName: model.fullName,
+            nickname: model.nickname,
+            hogwartsHouse: model.hogwartsHouse,
+            image: model.image,
+            birthdate: model.birthdate
         )
+        
     }
     
     func dtoToDomain(_ list: [CharacterDTO]? ) -> [CharacterDomain] {
