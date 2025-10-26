7942163a67ec32174523c3498474b0056955f2ae
YoussefAlaaeldin <yousufalaa10@yahoo.com>
2025-10-26 19:48:01 +0300

    Change CharacterDTO

--- PotterVerse/Features/Characters/Data/Model/DTO/CharacterDTO.swift
+++ PotterVerse/Features/Characters/Data/Model/DTO/CharacterDTO.swift
@@ -8,36 +8,11 @@
 import Foundation
 
 struct CharacterDTO: Codable {
-    let id, name: String
-    let alternateNames: [String]
-    let species: String
-    let gender: String?
-    let house: String?
-    let dateOfBirth: String?
-    let yearOfBirth: Int?
-    let wizard: Bool
-    let ancestry: String?
-    let eyeColour: String?
-    let hairColour: String?
-    let wand: Wand
-    let patronus: String?
-    let hogwartsStudent, hogwartsStaff: Bool
-    let actor: String
-    let alternateActors: [String]
-    let alive: Bool
+    let fullName, nickname: String
+    let hogwartsHouse: String
+    let interpretedBy: String
+    let children: [String]
     let image: String
-
-    enum CodingKeys: String, CodingKey {
-        case id, name
-        case alternateNames = "alternate_names"
-        case species, gender, house, dateOfBirth, yearOfBirth, wizard, ancestry, eyeColour, hairColour, wand, patronus, hogwartsStudent, hogwartsStaff, actor
-        case alternateActors = "alternate_actors"
-        case alive, image
-    }
-}
-// MARK: - Wand
-struct Wand: Codable {
-    let wood: String
-    let core: String?
-    let length: Double?
+    let birthdate: String
+    let index: Int
 }
