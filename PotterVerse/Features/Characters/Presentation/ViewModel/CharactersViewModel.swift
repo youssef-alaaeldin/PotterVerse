bd7189a261aa62251b9d092fe2b3eaa6d1b213e3
YoussefAlaaeldin <yousufalaa10@yahoo.com>
2025-10-26 14:06:34 +0300

    Add CharactersViewModel

---
+++ PotterVerse/Features/Characters/Presentation/ViewModel/CharactersViewModel.swift
@@ -0,0 +1,77 @@
+//
+//  CharactersViewModel.swift
+//  PotterVerse
+//
+//  Created by Yousuf Abdelfattah on 26/10/2025.
+//
+
+import Foundation
+import Combine
+import Factory
+
+class CharactersViewModel {
+    @Injected(\.getCharatersUseCase) private var getCharatersUseCase
+    
+    @Published var characters: [CharacterDomain]? = []
+    @Published var isCharactersLoading: Bool = false
+    @Published var charactersLoadingState: LoadingState<[CharacterDomain]> = .loading
+    
+    var myTasks: [Task<Void, Never>] = []
+    var cancellables = Set<AnyCancellable>()
+    
+    func cancelTasks() {
+        myTasks.forEach { $0.cancel() }
+    }
+    
+    init() {
+        fetchCharacters()
+    }
+    
+    deinit {
+        cancelTasks()
+    }
+}
+
+
+// MARK: Subscribe To Publishers
+extension CharactersViewModel {
+    func susbscribeToCharacters() {
+        $charactersLoadingState
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] state in
+                guard let self = self else { return }
+                switch state {
+                    case .loading:
+                        self.isCharactersLoading = true
+                        self.characters = CharacterDomain.mockList
+                    case .loaded(let data):
+                        self.isCharactersLoading = false
+                        self.characters = data
+                    case .error:
+                        self.isCharactersLoading = false
+                        self.characters = nil
+                }
+            }
+            .store(in: &cancellables)
+    }
+}
+
+// MARK: - API Calls
+extension CharactersViewModel {
+    func fetchCharacters() {
+        charactersLoadingState = .loading
+        
+        let charactersTask = Task {
+            do {
+                let result = try await getCharatersUseCase.execute(charactersRequest: CharactersRequest())
+                await MainActor.run {
+                    charactersLoadingState = .loaded(result)
+                }
+                
+            } catch {
+                print("Error")
+            }
+        }
+        myTasks.append(charactersTask)
+    }
+}
